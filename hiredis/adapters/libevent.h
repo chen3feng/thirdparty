bf58a03a2c501dd4bdcb05a98e2e29496f2c127e
qinzuoyan <qinzuoyan@gmail.com>
2015-04-12 14:10:03 +0800

    add thirdparty/hiredis

---
+++ hiredis/adapters/libevent.h
@@ -0,0 +1,12 @@
+// DO NOT EDIT! Geneated by link_hiredis_headers.sh, edit the script if necessary.
+//
+// Description: Link to hiredis-0.12.1 source header file
+
+#ifndef THIRDPARTY_HIREDIS_ADAPTERS_LIBEVENT_H
+#define THIRDPARTY_HIREDIS_ADAPTERS_LIBEVENT_H
+#pragma once
+
+#include "thirdparty/hiredis-0.12.1/adapters/libevent.h"
+
+#endif // THIRDPARTY_HIREDIS_ADAPTERS_LIBEVENT_H
+
---
+++ hiredis/async.h
@@ -0,0 +1,12 @@
+// DO NOT EDIT! Geneated by link_hiredis_headers.sh, edit the script if necessary.
+//
+// Description: Link to hiredis-0.12.1 source header file
+
+#ifndef THIRDPARTY_HIREDIS_ASYNC_H
+#define THIRDPARTY_HIREDIS_ASYNC_H
+#pragma once
+
+#include "thirdparty/hiredis-0.12.1/async.h"
+
+#endif // THIRDPARTY_HIREDIS_ASYNC_H
+
