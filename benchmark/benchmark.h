82d6ec8b59e3ce06889f70cad907d3bdb53bc7a8
CHEN Feng <chen3feng@gmail.com>
2018-12-24 11:56:09 -0500

    Add google benchmark

---
+++ benchmark/benchmark.h
@@ -0,0 +1,7 @@
+#ifndef THIRDPARTY_BENCHMARK_BENCHMARK_H
+#define THIRDPARTY_BENCHMARK_BENCHMARK_H
+#pragma once
+
+#include "thirdparty/benchmark-1.4.1/include/benchmark/benchmark.h"
+
+#endif // THIRDPARTY_BENCHMARK_BENCHMARK_H
